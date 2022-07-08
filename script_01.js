a49990704a83a664eabdef34f7109d5a1d7f3707
BilalU123 <uddinbilal@aol.com>
2022-07-08 10:32:50 +0200

    JS ist eine Untypisierte Sprache!

--- script_01.js
+++ script_01.js
@@ -14,10 +14,29 @@
 
 /***** 02 Deklaration + Wertzuweisung *****/
 
-let firstName; //Deklaration (Definition)
-firstName = "Max"; //Wertzuweisung
-console.log(firstName); //Ausgaben
+//let firstName; //Deklaration (Definition)
+//firstName = "Max"; //Wertzuweisung
+//console.log(firstName); //Ausgaben
 
-let familyName = "Mütze"; //Deklaration + Wertzuweisung
-console.log(familyName);   //Ausgaben
-console.log(firstName +" "+ familyName);
+//let familyName = "Mütze"; //Deklaration + Wertzuweisung
+//console.log(familyName);   //Ausgaben
+//console.log(firstName +" "+ familyName);
+
+/***** 02 Deklaration + Wertzuweisung 2 *****/
+
+//let firstName,familyName;
+//firstName = prompt("Bitte Vorname eingeben");
+//familyName = prompt("Bitte Nachname eingeben");
+//console.log(firstName +" "+ familyName);
+
+// JS ist eine Untypisierte Sprache! | untyped
+
+let test;
+test = "Hi";
+test = 2;
+test = true;
+
+
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: "+ test);

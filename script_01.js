fdf6fd60fff61722d4d237ecde0d33fde35d0c2a
BilalU123 <uddinbilal@aol.com>
2022-07-08 09:17:34 +0200

    Deklaration und Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -8,8 +8,16 @@
  * 
 */
 
-// Zeilen-Ausgaben
+// Konsolen-Ausgaben
 //console.log("Hallo");
 //console.log(firstName);
 
 /***** 02 Deklaration + Wertzuweisung *****/
+
+let firstName; //Deklaration (Definition)
+firstName = "Max"; //Wertzuweisung
+console.log(firstName); //Ausgaben
+
+let familyName = "Mütze"; //Deklaration + Wertzuweisung
+console.log(familyName);   //Ausgaben
+console.log(firstName +" "+ familyName);
